52c574ffe66dee2ac9191b2d4f47705e1ea0a19e
jrsahuquillo <jrsahuquillo@gmail.com>
2016-11-18 10:11:41 +0100

    Update products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,9 +11,12 @@ class ProductsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-        @user_product = @user.products.new(title: params[:product][:title],
-                                description: params[:product][:description],
-                                deadline: params[:product][:deadline])
+        @user_product = @user.products.new(product_params)
+
+                                # (title: params[:product][:title],
+                                # description: params[:product][:description],
+                                # deadline: params[:product][:deadline])
+                                    #Changed by strong params! -> product_params
 
         if @user_product.save
           redirect_to "/users/#{@user.id}/products"
@@ -25,6 +28,29 @@ class ProductsController < ApplicationController
   def edit
     @user = User.find params[:user_id]
     @user_product = @user.products.find params[:id]
+  end
+
+  def update
+    @user = User.find_by(id: params[:user_id])
+    @user_product = @user.products.find_by(id: params[:id])
+
+    if @user_product.update(product_params)
+
+                      # (title: params[:product][:title],
+                      # description: params[:product][:description],
+                      # deadline: params[:product][:deadline])
+                          #Changed by strong params! -> product_params
+
+      redirect_to "/users/#{@user.id}/products"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :deadline)
   end
 
 end
